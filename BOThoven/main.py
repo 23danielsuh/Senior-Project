35fa4630906fc30f8af5ae6678ddc7fad6b2ddcb
benpomeranets <ben.pomeranets@gmail.com>
2023-05-12 18:44:56 -0400

    added calculations for white keys

--- BOThoven/main.py
+++ BOThoven/main.py
@@ -1,5 +1,7 @@
 #from adafruit_servokit import ServoKit
 
+#kit = ServoKit(channels=16)
+
 import music21 as m
 
 from music21 import *
@@ -26,14 +28,13 @@ class Robot:
 
     def play_note(self, note):
 
-        self.move_motor(note - self.left_arm_position)
+        self.move_motor(note)
 
     def release(self, motorID, duration):
 
         time.sleep(duration - 0.05)
 
         print("Returning motor " + str(motorID))
-
 
 def open_file(xml_path):
 
@@ -43,56 +44,82 @@ def open_file(xml_path):
 
     return right_hand
 
+def get_pitch_boundaries(song):
+
+    lowestPitch = 60
+    highestPitch = 60
+
+    for note in song.recurse().notes:
+
+        if note.isNote:
+
+            if note.pitch.ps < lowestPitch:
+
+                lowestPitch = note.pitch.ps
+            
+            elif note.pitch.ps > highestPitch:
+
+                highestPitch = note.pitch.ps
+        
+        elif note.isChord:
+
+            for x in note._notes:
+
+                if x.pitch.ps < lowestPitch:
+
+                    lowestPitch = x.pitch.ps
+            
+                elif x.pitch.ps > highestPitch:
+
+                    highestPitch = x.pitch.ps
+        
+    return [lowestPitch, highestPitch]
+
+def get_white_keys(min_pitch, max_pitch):
+    # Create a range of pitches using music21's pitch class
+    pitch_range = range(int(min_pitch), int(max_pitch) + 1)
+    pitches = [pitch.Pitch(p) for p in pitch_range]
+    
+    # Filter out all the non-white keys
+    white_keys = [str(p) for p in pitches if str(p.accidental) == "natural"]
+
+    return white_keys
+
 def main():
 
     robot = Robot(60)
 
-    song = open_file("/Users/benpomeranets/Documents/GitHub/senior_project/data/test.mxl")
+    song = open_file("/Users/benpomeranets/Documents/GitHub/senior_project/data/twinkle.xml")
     
     song = song.stripTies()
 
-    song = song.voicesToParts(separateById = True)
+    whiteKeys = get_white_keys(get_pitch_boundaries(song)[0], get_pitch_boundaries(song)[1])
+    print(whiteKeys)
 
-    print(' '.join([p.nameWithOctave for p in song.parts[0].pitches]))
+    def playNotes():
 
-    print(' '.join([p.nameWithOctave for p in song.parts[1].pitches]))
-
-    def playNotes(executor, part):
-
-        for note in part.recurse().notes:
+        for note in song.recurse().notes:
 
             if note.isNote:
                 pitch = note.pitch.ps
-                duration = note.duration.quarterLength
-
-                robot.play_note(pitch)
-                executor.submit(robot.release, pitch - 60, duration)
+                duration = note.duration.quarterLength * (60 / BPM)
+                
+                robot.play_note(whiteKeys.index(str(note.pitch.name) + str(note.pitch.octave)))
+                robot.release(whiteKeys.index(str(note.pitch.name) + str(note.pitch.octave)), duration)
 
                 time.sleep(duration)
 
             if note.isChord:
                 for x in note._notes:
-                    pitch = note.pitch.ps
-                    duration = note.duration.quarterLength
+                    pitch = x.pitch.ps
+                    duration = x.duration.quarterLength * (60 / BPM)
 
                     robot.play_note(pitch)
-                    executor.submit(robot.release, pitch - 60, duration)
+                    robot.release(pitch - 60, duration)
 
                 time.sleep(duration)
-
-    #print("pitch, duration_string, duration, tie, midi pitch, octave")
-    with ThreadPoolExecutor() as executor:
-
-        for part in song.parts:
-
-            executor.submit(playNotes, executor, part)
-
-def getMusicProperties(x):
-    s = ""
-    s = str(x.name) + ", " + str(x.duration.quarterLength)
-    s += ", "
-    s += str(x.pitch.ps) + ", " + str(x.octave)
-    return s
+    
+    playNotes()
 
 if __name__ == "__main__":
     main()
