e5fd1acb0e1f53d872f3794527bd43f7fa8c6e10
Daniel Suh <23denial@gmail.com>
2023-05-13 00:00:17 -0400

    #red

--- BOThoven/main.py
+++ BOThoven/main.py
@@ -14,7 +14,7 @@ import time
 
 import tkinter as tk
 
-BPM = 120
+BPM = 80
 ANGLE = 30
 
 
@@ -26,33 +26,46 @@ class Robot:
     def __init__(self, init_left_position):
         self.left_arm_position = init_left_position
 
-    def move_motor(self, motorID):
+    def move_motor(self, motorID, accidental, window):
         while motorID < 0:
             motorID += 12
         while motorID >= 12:
             motorID -= 12
         print("Moving motor " + str(motorID))
+        
+        print('pressing' + str(int(motorID)) + "A")
+
+        if accidental == "natural":
+            window.canvas.itemconfig(str(int(motorID + 60)) + "A", fill="#FFA32B")
+        else:
+            window.canvas.itemconfig(str(int(motorID + 60)) + "A", fill="#C03A38")
+        
 
         # kit.servo[motorID].angle = ANGLE
 
-    def play_note(self, note, window):
-        self.move_motor(note)
+    def play_note(self, note, window, accidental):
+        self.move_motor(note, accidental, window)
 
         # Get the corresponding key_id for the played note
         # key_id = str(note)
 
         # Change the color of the key to green
 
-    def release(self, motorID, duration, window, pitch):
+    def release(self, motorID, duration, window, pitch, accidental):
         while motorID < 0:
             motorID += 12
         while motorID >= 12:
             motorID -= 12
 
         print("Returning motor " + str(motorID))
-
-        window.canvas.itemconfig(str(int(pitch)) + "A", fill="white")
-
+        
+        print('releasing' + str(int(motorID)) + "A")
+        
+        if accidental == "natural":
+            window.canvas.itemconfig(str(int(motorID + 60)) + "A", fill="white")
+        else:
+            window.canvas.itemconfig(str(int(motorID + 60)) + "A", fill="black") 
+        
         # kit.servo[motorID].angle = 0
 
     def translate(self, position):
@@ -72,13 +85,6 @@ def get_keys(min_pitch, max_pitch):
     pitch_range = range(min_pitch, max_pitch)
 
     pitches = [pitch.Pitch(p) for p in pitch_range]
-
-    """for i, x in reversed(list(enumerate(pitches))):
-        if pitches[i].name == "E" and pitches[i+1].name == "F" and i > 0:
-            pitches.insert(i+1, "FILLER")
-            
-        if pitches[i].name == "B" and pitches[i+1].name == "C" and i > 0:
-            pitches.insert(i+1, "FILLER")"""
 
     pitches = [str(p) for p in pitches]
 
@@ -114,27 +120,32 @@ def playNotes(part, robot, window):
 
         if type(el) == note.Note:
             pitch = el.pitch.ps
+            
+            if pitch % 12 in [1, 3, 6, 8, 10]:
+                accidental = "black"
+            else:
+                accidental = "natural"
+            
+            #accidental = "natural"
 
             duration = el.duration.quarterLength * (60 / BPM)
 
             robot.play_note(
-                keys.index(str(el.pitch.name) + str(el.pitch.octave))
-                - robot.left_arm_position,
+                pitch - 60,
                 window,
+                accidental
             )
-
-            window.canvas.itemconfig(str(int(pitch)) + "A", fill="#e41e15")
 
             print("Playing " + (el.pitch.name) + str(el.pitch.octave))
 
             time.sleep(duration - 0.075)
 
             robot.release(
-                keys.index(str(el.pitch.name) + str(el.pitch.octave))
-                - robot.left_arm_position,
+                pitch - 60,
                 duration,
                 window,
                 pitch,
+                accidental
             )
 
             time.sleep(0.075)
@@ -145,27 +156,30 @@ def playNotes(part, robot, window):
             for x in el._notes:
                 pitch = x.pitch.ps
 
+                if pitch % 12 in [1, 3, 6, 8, 10]:
+                    accidental = "black"
+                else:
+                    accidental = "natural"
+                    
+                #accidental = "natural"
+                
                 duration = x.duration.quarterLength * (60 / BPM)
 
                 robot.play_note(
-                    keys.index(str(x.pitch.name) + str(x.pitch.octave))
-                    - robot.left_arm_position,
+                    pitch - 60,
                     window,
+                    accidental
                 )
-
-                window.canvas.itemconfig(str(int(pitch)) + "A", fill="#e41e15")
-
-                print("Playing " + (x.pitch.name) + str(x.pitch.octave))
 
             time.sleep(duration - 0.075)
 
             for x in el._notes:
                 robot.release(
-                    keys.index(str(x.pitch.name) + str(x.pitch.octave))
-                    - robot.left_arm_position,
+                    x.pitch.ps - 60,
                     duration,
                     window,
                     x.pitch.ps,
+                    accidental
                 )
 
             time.sleep(0.075)
@@ -239,6 +253,9 @@ class PianoWindow(tk.Tk):
                 self.keyPos.append([x, note, False, "white"])
 
                 self.xPos += keyWidth
+                
+        #for x in self.keyPos:
+            #print(x[1])
 
         for key in self.keyPos:
             if key[3] == "white":
@@ -253,7 +270,6 @@ class PianoWindow(tk.Tk):
                 )
 
         for key in self.keyPos:
-            tag = str(key[1] - self.startNote)
             if key[3] == "black":
                 self.canvas.create_rectangle(
                     key[0],
@@ -268,10 +284,10 @@ class PianoWindow(tk.Tk):
 
 def main():
     robot = Robot(get_keys(36, 85).index("C4"))
-    # song = open_file("../data/twinkle.xml")
-    # song = open_file("../BOThoven/difficult_test.xml")
-    # song = open_file("../data/fur_elise.mxl")
-    song = open_file("../data/one_octave.mxl")
+    #song = open_file("../data/twinkle.xml")
+    #song = open_file("../BOThoven/difficult_test.xml")
+    song = open_file("../data/fur_elise.mxl")
+    #song = open_file("../data/one_octave.mxl")
     song = song.stripTies()
     song = song.voicesToParts()
 
@@ -279,6 +295,7 @@ def main():
         window = PianoWindow(24, 84)
         for part in song:
             a = executor.submit(playNotes, part, robot, window)
+            #print(a.result())
         os.environ["TK_SILENCE_DEPRECATION"] = "1"
         os.environ["DISPLAY"] = ":0"
         window.draw()
