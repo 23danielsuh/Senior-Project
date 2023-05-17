35e45219fc6f30596447e7c799d81a54654866ed
benpomeranets <ben.pomeranets@gmail.com>
2023-05-16 22:52:34 -0400

    created a piano graphic, testing the key highlights

--- BOThoven/main.py
+++ BOThoven/main.py
@@ -1,6 +1,6 @@
-from adafruit_servokit import ServoKit
+#from adafruit_servokit import ServoKit
 
-kit = ServoKit(channels=16)
+#kit = ServoKit(channels=16)
 
 import music21 as m
 
@@ -29,19 +29,50 @@ class Robot:
 
     def move_motor(self, motorID):
 
-        #print("Moving motor " + str(motorID))
+        print("Moving motor " + str(motorID))
         
-        kit.servo[motorID].angle = ANGLE
+        #kit.servo[motorID].angle = ANGLE
 
-    def play_note(self, note):
+    def play_note(self, note, window):
 
         self.move_motor(note)
-
-    def release(self, motorID, duration):
-
-        #print("Returning motor " + str(motorID))
         
-        kit.servo[motorID].angle = 0
+        window.canvas.itemconfig("30", fill="red")
+        window.canvas.itemconfig("31", fill="red")
+        window.canvas.itemconfig("32", fill="red")
+        window.canvas.itemconfig("33", fill="red")
+        window.canvas.itemconfig("34", fill="red")
+        window.canvas.itemconfig("35", fill="red")
+        window.canvas.itemconfig("36", fill="red")
+        window.canvas.itemconfig("37", fill="red")
+        window.canvas.itemconfig("38", fill="red")
+        window.canvas.itemconfig("39", fill="red")
+        window.canvas.itemconfig("40", fill="red")
+        window.canvas.itemconfig("41", fill="red")
+
+        # Get the corresponding key_id for the played note
+        #key_id = str(note)
+
+        # Change the color of the key to green
+
+    def release(self, motorID, duration, window):
+
+        print("Returning motor " + str(motorID))
+
+        window.canvas.itemconfig("30", fill="white")
+        window.canvas.itemconfig("31", fill="white")
+        window.canvas.itemconfig("32", fill="white")
+        window.canvas.itemconfig("33", fill="white")
+        window.canvas.itemconfig("34", fill="white")
+        window.canvas.itemconfig("35", fill="white")
+        window.canvas.itemconfig("36", fill="white")
+        window.canvas.itemconfig("37", fill="white")
+        window.canvas.itemconfig("38", fill="white")
+        window.canvas.itemconfig("39", fill="white")
+        window.canvas.itemconfig("40", fill="white")
+        window.canvas.itemconfig("41", fill="white")
+    
+        #kit.servo[motorID].angle = 0
         
     def translate(self, position):
         self.left_arm_position = position
@@ -75,7 +106,7 @@ def get_keys(min_pitch, max_pitch):
 
     return pitches
 
-def playNotes(part, robot):  
+def playNotes(part, robot, window):  
     keys = get_keys(36, 85)
     print(keys)
     
@@ -95,104 +126,117 @@ def playNotes(part, robot):
 
             duration = el.duration.quarterLength * (60 / BPM)
             
-            robot.play_note(keys.index(str(el.pitch.name) + str(el.pitch.octave)) - robot.left_arm_position)
+            robot.play_note(keys.index(str(el.pitch.name) + str(el.pitch.octave)) - robot.left_arm_position, window)
             
             print("Playing " + (el.pitch.name) + str(el.pitch.octave))
             
             time.sleep(duration - 0.075)
 
-            robot.release(keys.index(str(el.pitch.name) + str(el.pitch.octave)) - robot.left_arm_position, duration)
+            robot.release(keys.index(str(el.pitch.name) + str(el.pitch.octave)) - robot.left_arm_position, duration, window)
 
             time.sleep(0.075)
                 
         if type(el) == chord.Chord:
+
+            print("playing chord")
+
             for x in el._notes:
 
                 pitch = x.pitch.ps
 
                 duration = x.duration.quarterLength * (60 / BPM)
                 
-                robot.play_note(keys.index(str(x.pitch.name) + str(x.pitch.octave)) - robot.left_arm_position)
+                robot.play_note(keys.index(str(x.pitch.name) + str(x.pitch.octave)) - robot.left_arm_position, window)
                 
                 print("Playing " + (x.pitch.name) + str(x.pitch.octave))
 
             time.sleep(duration - 0.075)
             
             for x in el._notes:
-                robot.release(keys.index(str(x.pitch.name) + str(x.pitch.octave)) - robot.left_arm_position, duration)
+
+                robot.release(keys.index(str(x.pitch.name) + str(x.pitch.octave)) - robot.left_arm_position, duration, window)
+
             time.sleep(0.075)
 
         elif type(el) == note.Rest:
             duration = el.duration.quarterLength * (60 / BPM)
             time.sleep(duration)
             print(f"Resting for {duration} seconds")
+    
+    print("done")
+
+class PianoWindow(tk.Tk):
+
+    def __init__(self, startNote, endNote):
+        super().__init__()
+
+        numWhiteKeys = 0
+
+        for key in range(startNote, endNote + 1):
+
+            if not key % 12 in [1, 3, 6, 8, 10]:
+
+                numWhiteKeys += 1
+
+        self.width = self.winfo_screenwidth()
+        self.height = self.winfo_screenheight()
+
+        self.whiteKeyWidth = self.width * 0.02
+        self.whiteKeyHeight = self.height * 0.14
+
+        self.pianoWidth = numWhiteKeys * self.whiteKeyWidth
+
+        self.blackKeyWidth = self.width * 0.02 * (13.5 / 23.5)
+        self.blackKeyHeight = self.blackKeyWidth * 5
+        
+        self.title("BOThoven UI")
+        self.geometry("{0}x{1}+0+0".format(self.width, self.height))  # Set initial size and position
+
+        self.configure(bg="white")
+
+        self.canvas = tk.Canvas(self, width=self.winfo_screenwidth(), height=self.winfo_screenheight(), bg="#D3D3D3")
+        self.canvas.pack()  # Fill and expand to fill the entire window
+
+        keyWidth = 0
+        keyHeight = 0
+        xPos = (self.width - self.pianoWidth) / 2
+        yPos = (self.height * 3/4) - self.whiteKeyHeight
+
+        blackKeyPos = []
+
+        for note in range(startNote, endNote + 1):
+            x = xPos
+            if note % 12 in [1, 3, 6, 8, 10]:
+                blackKeyPos.append([x, note])
+
+            else:
+                color = 'white'
+                keyWidth = self.whiteKeyWidth
+                keyHeight = self.whiteKeyHeight
+                key = self.canvas.create_rectangle(x, yPos, x + keyWidth, yPos + keyHeight, fill=color, outline='black')
+                self.canvas.itemconfig(key, tags=str(note))  # Add 'key' tag to identify white keys 
+
+                xPos += keyWidth
+        
+        for pos in blackKeyPos:
+            color = 'black'
+            keyWidth = self.blackKeyWidth
+            keyHeight = self.blackKeyHeight
+            key = self.canvas.create_rectangle(pos[0] - keyWidth / 2, yPos, pos[0] + keyWidth / 2, yPos + keyHeight, fill=color, outline='black')
+            self.canvas.itemconfig(key, tags=str(pos[1]))
 
 def main():
     robot = Robot(get_keys(36, 85).index("C4"))
-
-    song = open_file("../data/difficult_test.xml")
-    
+    song = open_file("../BOThoven/difficult_test.xml")
     song = song.stripTies()
-    
     song = song.voicesToParts()
-    
-    def musicThread():
-        playNotes(part, robot)
-        
-    def windowThread():
-        os.environ["DISPLAY"] = ":0"
-        window = tk.Tk()
-        window.title("BOThoven UI")
-        screen_width = window.winfo_screenwidth()
-        screen_height = window.winfo_screenheight()
-        
-        window.geometry(f"{screen_width}x{screen_height}")
-        
-        startNote = 24
-        endNote = 84
-        
-        xPos = 0
-                
-        canvas = tk.Canvas(window, width=screen_width, height=screen_height)
-        canvas.pack()
-        
-        keyWidth = 0
-        
-        keyHeight = 0
-        
-        for note in range(startNote, endNote + 1):
-            
-            x = xPos
-            
-            if note % 12 in [1, 3, 6, 8, 10]:
-            
-                color = 'black'
-                
-                keyWidth = 27
-                
-                keyHeight = 200
-            
-            else:
-                
-                color = 'white'
-                
-                keyWidth = 47
-                
-                keyHeight = 300
-                
-            canvas.create_rectangle(x, 0, x + xPos, keyHeight, fill=color, outline='black')
-            
-            xPos += keyWidth
-        
-        window.mainloop()    
-        
+
     with ThreadPoolExecutor() as executor:
-        #for part in song.getElementsByClass("Part"):
-            #a = executor.submit(musicThread)
-        
-        a = executor.submit(windowThread)        
-        print(a.result())
-    
+        window = PianoWindow(24, 84)
+        a = executor.submit(playNotes, song, robot, window)
+        os.environ['TK_SILENCE_DEPRECATION'] = '1'
+        os.environ["DISPLAY"] = ":0"
+        window.mainloop()
 
 if __name__ == "__main__":
     main()
